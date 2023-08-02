65e8103ddb4794150e0b19a447e432b1f1c39bbe
wookyungan <aqualiquid1@gmail.com>
2023-08-02 21:17:28 +0900

    leetcode easy

---
+++ gx/jumping_frog.py
@@ -0,0 +1,13 @@
+import collections
+
+"""
+class Solutions:
+    def min_jumping_trial(self, N:int, K:int)-> int:
+        dp = [0] * N
+        start=[]
+        start[0] = dp[0]
+        for i in range(1,N):
+            crnt_jmp =
+
+        return 0
+"""
