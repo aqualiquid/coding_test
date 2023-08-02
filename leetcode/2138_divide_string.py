9f693886894a952d61eba1801a36f34f557b8ea7
wookyungan <aqualiquid1@gmail.com>
2023-08-02 21:17:43 +0900

    leetcode easy

--- leetcode/2138_divide_string.py
+++ leetcode/2138_divide_string.py
@@ -7,7 +7,7 @@ class Solutions:
         for i in range(0, len(s), k):
             group = s[i:i+k]
             if len(group) < k:
-                group += fill * (k - len(group))
+                group += fill * (k-len(group))
             groups.append(group)
         return groups
 
