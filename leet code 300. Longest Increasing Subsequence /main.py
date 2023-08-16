37c8b721848fae1cd803f70311402f45af16b2b7
wookyungan <aqualiquid1@gmail.com>
2023-08-16 14:20:28 +0900

    leetcode medium (modified)

--- leet code 300. Longest Increasing Subsequence /main.py
+++ leet code 300. Longest Increasing Subsequence /main.py
@@ -1,20 +1,12 @@
 from typing import List
-from bisect import bisect_left
-
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [float('inf')] * (len(nums)+1)
-        dp[0] = nums[0]
+        if not nums:
+            return 0
 
-        for i in range(1, len(dp)):
-            length = 0
-            for j in range(i, len(dp)):
-                if nums[i]< nums[j]:
-                    dp[i] = length+1
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
-
-"""
-Input: nums = [10,9,2,5,3,7,101,18]
-Output: 4
-Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
-"""
