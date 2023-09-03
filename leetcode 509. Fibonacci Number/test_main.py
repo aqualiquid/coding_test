1b426cf39cf0ad5719f06f932e1b82f6aabb1535
wookyungan <aqualiquid1@gmail.com>
2023-09-03 14:28:13 +0900

    leetcode easy (fibonacci)

---
+++ leetcode 509. Fibonacci Number/test_main.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from main import Solution
+
+
+class TestSolution(TestCase):
+    def test1_Fibonacci_Numbe(self):
+        solution = Solution()
+        self.assertEqual(
+            1,
+            solution.fib(
+            2
+            )
+        )
+
+    def test2_Fibonacci_Numbe(self):
+        solution = Solution()
+        self.assertEqual(
+            3,
+            solution.fib(
+            4
+            )
+        )
+
+    def test3_Fibonacci_Numbe(self):
+        solution = Solution()
+        self.assertEqual(
+            2,
+            solution.fib(
+            3
+            )
+        )
