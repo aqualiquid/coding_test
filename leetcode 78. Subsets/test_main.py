e5ccbd8787c2c4fe36e41c0393ddefdad1b22934
wookyungan <aqualiquid1@gmail.com>
2023-09-03 14:51:25 +0900

    leetcode medium (subsets recursive way for learning backtracking)

--- leetcode 78. Subsets/test_main.py
+++ leetcode 78. Subsets/test_main.py
@@ -1,4 +1,22 @@
 from unittest import TestCase
-from main import Solution:
+from main import Solution
 
 class TestSolution(TestCase):
+    def test1_subsets(self):
+        solution = Solution()
+        self.assertEqual(
+            {frozenset()},
+            {frozenset(x) for x in solution.subsets([])}
+        )
+
+        self.assertEqual(
+            {frozenset(), frozenset([1]), frozenset([2]), frozenset([1,2]), frozenset([3]), frozenset([1,3]), frozenset([2,3]), frozenset([1,2,3])},
+            {frozenset(x) for x in solution.subsets([1, 2, 3])}
+        )
+
+    def test2_subsets(self):
+        solution=Solution()
+        self.assertEqual(
+            {frozenset(), frozenset([0])},
+            {frozenset(x) for x in solution.subsets([0])}
+        )
