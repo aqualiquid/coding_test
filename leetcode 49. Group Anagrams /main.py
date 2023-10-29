80998c932657d8986891a8d349094aea69060125
wookyungan <aqualiquid1@gmail.com>
2023-10-29 19:14:15 +0900

    leetcode group anagram (medium)

---
+++ leetcode 49. Group Anagrams /main.py
@@ -0,0 +1,11 @@
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            anagrams[sorted_word].append(word)
+
+        return anagrams.values()
