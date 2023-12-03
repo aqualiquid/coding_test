739c8a9b804644e58d29b0302046e01e62e2e1f9
wookyungan <aqualiquid1@gmail.com>
2023-12-03 17:19:34 +0900

    leetcode easy merge two sorted list (linked list)

---
+++ leecode 21. Merge Two Sorted Lists/main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        elif not list1 and list2:
+            return None
+        else:
+            if list1.val > list2.val:
+                list2.next = self.mergeTwoLists(list1, list2.next)
+                return list2
+            else:
+                list1.next = self.mergeTwoLists(list1.next, list2)
+                return list1
