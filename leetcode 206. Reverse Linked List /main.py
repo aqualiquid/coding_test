c60ead03ffcbd39d3f9b3645a16b4992346a2d24
wookyungan <aqualiquid1@gmail.com>
2023-12-03 17:16:46 +0900

    leetcode easy reversaeList

---
+++ leetcode 206. Reverse Linked List /main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        prev, current = None, head
+
+        while current:
+            # swap the pointer
+            tmp = current.next
+            current.next = prev
+
+            prev = current
+            current = tmp
+
+        return prev
