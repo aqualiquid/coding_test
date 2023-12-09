45c121306d98e337e63d9d3bb61cdc125cbeb89f
wookyungan <aqualiquid1@gmail.com>
2023-12-09 11:46:03 +0900

    leetcode medium (linked list)

---
+++ leetcode 2. Add Two Numbers/main.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = head = ListNode(0)
+        carry = 0
+
+        # 셋 중에 하나라도 있을 경우 반복문
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        return root.next
