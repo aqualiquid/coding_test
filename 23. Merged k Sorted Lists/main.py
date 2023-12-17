03e730c7ed2ee9dab2206e87d4ad761f2f2ced25
wookyungan <aqualiquid1@gmail.com>
2023-12-17 19:53:59 +0900

    leetcode medium

---
+++ 23. Merged k Sorted Lists/main.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import Optional, List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        min_heap = []
+
+        for i, lst in enumerate(lists):
+            if lst:
+                heapq.heappush(min_heap, (lst.val, i, lst))
+
+        dummy = ListNode(0)
+        current = dummy
+
+        # get a smallist element
+        while min_heap:
+            val, i, node = heapq.heappop(min_heap)
+            current.next = node
+            current = current.next
+
+            if node.next:
+                heapq.heappush(min_heap,
+                               (node.next.val, i, node.next))
+
+        return dummy.next
