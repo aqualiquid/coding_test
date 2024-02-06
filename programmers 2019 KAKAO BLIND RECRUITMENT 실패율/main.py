477f0dcc7a52a900d5956c0ab5a9f0ad12a45f10
wookyungan <aqualiquid1@gmail.com>
2024-02-06 14:51:12 +0900

    programmers 2019 KAKAO BLIND RECRUITMENT 실패율

--- programmers 2019 KAKAO BLIND RECRUITMENT 실패율/main.py
+++ programmers 2019 KAKAO BLIND RECRUITMENT 실패율/main.py
@@ -1,12 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42889
-def solution(N, stages):
-    result = {}
-    denominator= len(stages)#stage의 개수 정의
-    for stage in range(1,N+1): #stage 범위 반복문
-        if denominator!=0: #denominator가 0이 아닌경우
-            count = stages.count(stage)#stage에 들어오는 숫자와 같은 숫자의 개수를 센다
-            result[stage]=count/denominator
-            denominator-=count
-        else:
-            result[stage]=0
-    return sorted(result,key=lambda x: result[x],reverse=True)
+class Soltuion:
+    def solution(N, stages):
+        result = {}
+        denominator= len(stages)#stage의 개수 정의
+        for stage in range(1,N+1): #stage 범위 반복문
+            if denominator!=0: #denominator가 0이 아닌경우
+                count = stages.count(stage)#stage에 들어오는 숫자와 같은 숫자의 개수를 센다
+                result[stage]=count/denominator
+                denominator-=count
+            else:
+                result[stage]=0
+        return sorted(result,key=lambda x: result[x],reverse=True)
+
