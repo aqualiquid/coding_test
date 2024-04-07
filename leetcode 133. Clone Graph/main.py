56873b917a9598e18d8373c8a833bf6b36897e78
wookyungan <aqualiquid1@gmail.com>
2024-04-07 19:28:44 +0900

    eliminate comments block

--- leetcode 133. Clone Graph/main.py
+++ leetcode 133. Clone Graph/main.py
@@ -1,10 +1,10 @@
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 
 from typing import Optional
 import collections
