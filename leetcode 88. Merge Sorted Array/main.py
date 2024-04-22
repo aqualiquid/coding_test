7a03b2a04269a33923e29ee250358250ab498299
wookyungan <aqualiquid1@gmail.com>
2024-04-23 07:50:53 +0900

    changed answer (TOP 1/150)

--- leetcode 88. Merge Sorted Array/main.py
+++ leetcode 88. Merge Sorted Array/main.py
@@ -1,26 +1,28 @@
 from typing import List
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if n == 0: return
-        # set 2 pointers
-        p1, p2 = m - 1, n - 1
-        # end of nums1 idx, b/c length of the len1 is (m+n)
-        p = m + n - 1
+        if n==0: return
+        # Setup Pointers: Use three pointers:
+        # i for indexing nums1 from the last initialized element (i.e., m-1).
+        # j for indexing nums2 from the last element (i.e., n-1).
+        # k for placing elements in the correct position
+            # in nums1, starting from the end (i.e., m+n-1).
+        i, j, k = m-1, n-1, m+n-1
+        # Merge nums1 and nums2 in reverse order
+        while i>=0 and j>=0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -=1
+            else : # nums1[i] < nums2[j]
+                nums1[k] =nums2[j]
+                j -=1
+            k-=1
 
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
-
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p2 -= 1
-            p1 -= 1
+        #Fill nums1 with remaining elements from nums2 if any
+        while j>=0:
+            nums1[k] =nums2[j]
+            i-=1
+            j-=1
